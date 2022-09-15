c58d66db7a42e3120704697cfba209376b82fb7b
jjfork <jakubiakrjulian@gmail.com>
2022-09-15 03:24:31 +0200

    new files

---
+++ udemyseleniumcs/alertbox.cs
@@ -0,0 +1,43 @@
+﻿//using OpenQA.Selenium;
+//using OpenQA.Selenium.Chrome;
+
+//class EntryPoint
+//{
+//    static IWebDriver driver = new ChromeDriver();
+//    static IAlert alert;
+//    static IWebElement image;
+//    static void Main()
+//    {
+//        string url = "https://testing.todorvachev.com/alert-box/";
+
+//        driver.Navigate().GoToUrl(url);
+
+//        alert = driver.SwitchTo().Alert();
+
+//        Console.WriteLine(alert.Text);
+
+//        alert.Accept();
+
+//        image = driver.FindElement(By.CssSelector("#post-119 > div > figure > img"));
+
+
+//        try
+//        {
+//            if (image.Displayed)
+//            {
+//                Console.WriteLine("ISEE!");
+//            }
+//        }
+//        catch (NoSuchElementException)
+//        {
+//            Console.WriteLine("IDONTSEEEEEEEE!");
+//        }
+
+//        Thread.Sleep(3000);
+
+//        driver.Quit();
+
+//    }
+
+
+//}
---
+++ udemyseleniumcs/dropdownmenu.cs
@@ -0,0 +1,46 @@
+﻿//using OpenQA.Selenium;
+//using OpenQA.Selenium.Chrome;
+
+//class EntryPoint
+//{
+//    static IWebDriver driver = new ChromeDriver();
+//    static IWebElement dropDownMenu;
+//    static IWebElement elementFromTheDropDownMenu;
+
+
+//    static void Main()
+//    {
+
+//        string url = "https://testing.todorvachev.com/drop-down-menu-test/";
+//        string dropDownMenuElements = "#post-6 > div > p:nth-child(6) > select > option:nth-child(3)";
+
+//        driver.Navigate().GoToUrl(url);
+
+//        dropDownMenu = driver.FindElement(By.Name("DropDownTest"));
+
+//        Console.WriteLine("The selected value is: " + dropDownMenu.GetAttribute("value"));
+
+//        elementFromTheDropDownMenu = driver.FindElement(By.CssSelector(dropDownMenuElements));
+
+//        Console.WriteLine("The third option from the drop down menu is: " + elementFromTheDropDownMenu.GetAttribute("value"));
+
+//        elementFromTheDropDownMenu.Click();
+
+//        Console.WriteLine("The selected value is: " + dropDownMenu.GetAttribute("value"));
+
+//        for (int i = 1; i <= 4; i++)
+//        {
+//            dropDownMenuElements = "#post-6 > div > p:nth-child(6) > select > option:nth-child(" + i + ")";
+//            elementFromTheDropDownMenu = driver.FindElement(By.CssSelector(dropDownMenuElements));
+
+//            Console.WriteLine("The " + i + " option from the drop down menu is: " + elementFromTheDropDownMenu.GetAttribute("value"));
+//        }
+
+//        Thread.Sleep(15000);
+
+//        driver.Quit();
+
+//    }
+
+
+//}
